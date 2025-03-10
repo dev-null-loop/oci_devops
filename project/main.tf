25a051e3330a4646a27e47ffee73007cb0657360
dev-null-loop <dev.null.loop@gmail.com>
2025-03-10 07:50:05 +0200

    devops:project:typo

--- project/main.tf
+++ project/main.tf
@@ -2,7 +2,7 @@ resource "oci_devops_project" "this" {
   compartment_id = var.compartment_id
   name           = var.name
   notification_config {
-    topic_id = oci_ons_notification_topic.this.id
+    topic_id = var.topic_id
   }
   defined_tags  = var.defined_tags
   description   = var.description
